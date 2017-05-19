81c9c96a559059df02a63c3df3941bc78d5ba7e7
ohkhaila <khaila.jones@lessannoyingcrm.com>
2017-05-19 12:02:00 -0500

    5.19

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <html>
     <head>
         <title>My website</title>
-        <link rel='stylesheet' href='style.css'/>
+               <link rel="stylesheet" href="style.css?Time=<?php echo microtime()?>" />
     </head>
     <body style>
-        <h1 style> Hi, welcome to my website! </h1>
+        <h1 style> Welcome to The Regular Girl Life Guide! </h1>
             <div style>
-        <p> Thanks for visiting my site. I'll explain a bit about myself, and then you can read some blog posts I've written </p>
+                
+
+
+
+        <p style>Thanks for joining us! Here everyone is welcome! We are all on a journey towards understanding, appreciation, and unity of ourselves.
+        Here we are working to improve ourselves which will end in countless improvements in every aspect of our lives.  </p>
         <h2 style>About me</h2>
         <p> My name is Khaila Jones, I've been alive for a whopping 20 years! I'm a student in St. Louis, Missouri. I'm currently learning to code. I have various interests and stuff.
             <h3 style>My blog</h3>
