bcca37129b3df6477ab7a67fcad4071b62e4ff8a
ohkhaila <khaila.jones@lessannoyingcrm.com>
2017-08-10 10:27:40 -0500

    play website

---
+++ yes/index.php
@@ -0,0 +1,11 @@
+<html>
+    <title>Kai Kode</title>
+        <h1>Welcome to my Website!</h1>
+            <body>
+                Here are a few pictures of me at one of my favorite places in the world... The Butterfly House!
+                    <img src="/images/butterflyBench.jpg" alt="Kai on Butterfly House Bench" style="display: block; padding: 15px; margin-left:auto; margin-right:auto; height: 50vh;" />
+                    <img src="/images/flowerBench.jpg" alt="Kai Butterfly House" style="display: block; margin-right:auto; margin-right:auto; height: 50vh;"/>
+                </menu>
+
+            </body>
+</html>
